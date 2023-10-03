b29d407c12787e8c5b60ef0001efbdf4755901eb
rjamal-av <141849755+rjamal-av@users.noreply.github.com>
2023-10-02 23:30:42 -0400

    Update main.tf for Connection Policy
    Update main.tf for Connection Policy

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ module "ace_automation" {
   aws_region                = var.aws_region
 }
 
-# resource "aviatrix_segmentation_network_domain_connection_policy" "ace_automation" {
-#   domain_name_1 = "bu1"
-#   domain_name_2 = "bu2"
-#   depends_on    = [module.ace_automation]
-# }
+resource "aviatrix_segmentation_network_domain_connection_policy" "ace_automation" {
+   domain_name_1 = "bu1"
+   domain_name_2 = "bu2"
+   depends_on    = [module.ace_automation]
+ }
